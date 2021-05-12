95228e217ca6854c3070abb241201c669dabc1f5
Dzarek <71340984+Dzarek@users.noreply.github.com>
2021-05-12 23:33:02 +0200

    Add files via upload

---
+++ WEBPACK/config/webpack.config.js
@@ -0,0 +1,56 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    main: "./src/index.js",
+  },
+  output: {
+    filename: "js/[name].js",
+    path: path.resolve(__dirname, "../", "build"),
+  },
+  devServer: {
+    open: true,
+    contentBase: path.resolve(__dirname, "../", "public"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.txt$/,
+        use: "raw-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(scss|sass)$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(jpg|png|svg|gif|jpeg)$/,
+        use: "file-loader",
+      },
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: [
+            ["@babel/preset-env", { useBuiltIns: "usage", corejs: "2.0.0" }],
+          ],
+          plugins: ["@babel/plugin-proposal-class-properties"],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HTMLWebpackPlugin({
+      title: "nowa apka",
+      template: "src/templates/template.html",
+    }),
+  ],
+};
---
+++ WEBPACK/config/webpack.config.prod.js
@@ -0,0 +1,108 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: {
+    main: "./src/index.js",
+  },
+  output: {
+    filename: "js/[name]-[contenthash:6].js",
+    path: path.resolve(__dirname, "../", "build"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.txt$/,
+        use: "raw-loader",
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [[require("autoprefixer")]],
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(scss|sass)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [[require("autoprefixer")]],
+              },
+            },
+          },
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(jpg|png|svg|gif|jpeg)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name][contenthash:6].[ext]",
+              outputPath: "images",
+            },
+          },
+          {
+            loader: "image-webpack-loader",
+            options: {
+              pngquant: {
+                quality: [0.65, 0.9],
+                progressive: true,
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: [
+            ["@babel/preset-env", { useBuiltIns: "usage", corejs: "2.0.0" }],
+          ],
+          plugins: ["@babel/plugin-proposal-class-properties"],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HTMLWebpackPlugin({
+      title: "nowa apka",
+      template: "src/templates/template.html",
+      minify: {
+        collapseWhitespace: true,
+      },
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name]-[contenthash:6].css",
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "public/images",
+          to: "images",
+        },
+      ],
+    }),
+  ],
+};
